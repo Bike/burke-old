dfcbf00d86b592d9b946fa7dfc581baab5bf1db9
James Kalenius <aeshtaer@gmail.com>
2014-12-30 18:04:47 -0800

    hack in a thing to prevent infinite EOF loop
    Previously if the main loop ran into an EOF, it would ask for input again. This
    was mildly annoying at a REPL, but in a file it means it continues asking the
    file for more input, so, well, "unexpected EOF" over and over.
    
    The fix special cases the error in the main error handler. This is a shitty
    version of handler-case.

--- main.c
+++ main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <setjmp.h>
+#include <string.h> // strcmp
+#include <stdlib.h> // exit
 #include "read.h"
 #include "write.h"
 #include "lisp.h"
@@ -7,17 +9,19 @@
 
 jmp_buf err_jmp;
 
-lispobj* jump(const char* format, ...) {
+lispobj* jump_with_eof(const char* format, ...) {
   va_list vargs;
   va_start(vargs, format);
   vfprintf(stderr, format, vargs);
   va_end(vargs);
+  if (!strcmp(format, "unexpected EOF\n")) // ugh
+    exit(1);
   longjmp(err_jmp, 0);
 }
 
 int main(void) {
   initialize_globals();
-  error = jump; // set up error handler
+  error = jump_with_eof; // set up error handler
   setjmp(err_jmp); // don't care about return
   while(1) {
     write_lisp(eval(read_lisp(stdin), ground_environment), lstdout);
